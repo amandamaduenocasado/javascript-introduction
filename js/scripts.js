2673a8526e4ccd0099b360f00c9f19a81c92feb2
Andrea <amandacaso97@gmail.com>
2024-10-16 14:55:34 +0200

    actualizacion

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+/* console.log(active); 
+
+muestra en el menu consola lo que escribamos aquí pero no se ejecuta */
+
+function sayHello(name) {
+  console.log('hola ' + name);
+}
+
+sayHello('amanda');
+
+function calculateSquareArea(side) {
+  console.log('El area del cuadrado es ' + side * side);
+}
+
+calculateSquareArea(25);
+
+function calculateTriangleArea(base, altura) {
+  console.log('El area del triangulo es ' + (base * altura) / 2);
+}
+
+calculateTriangleArea(10, 5);
